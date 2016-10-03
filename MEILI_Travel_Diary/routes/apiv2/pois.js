39e4dc0f1575ec3c74b59d3074a4b79610611a22
sweco-semhul <martin.hultman@sweco.se>
2016-10-03 13:35:20 +0200

    Adding empty route to pois.js to make app start

--- MEILI_Travel_Diary/routes/apiv2/pois.js
+++ MEILI_Travel_Diary/routes/apiv2/pois.js
@@ -1,7 +1,11 @@
 /**
  * Created by adi on 2016-10-03.
  */
+var express = require('express');
+var router = express.Router();
 
 // TODO - insert transition POIs
 
 // TODO - insert destination POIs
+
+module.exports = router;
