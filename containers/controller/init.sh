b5a1df4e0ef19a9d7d6a821fad661da2646343cf
sweco-semhul <martin.hultman@sweco.se>
2016-10-06 09:58:14 +0200

    Updating database name since it changed in server

--- containers/controller/init.sh
+++ containers/controller/init.sh
@@ -3,4 +3,4 @@ rm -rf /MEILI_Database
 git clone https://github.com/Badger-MEILI/MEILI-Database /MEILI_Database
 cd /MEILI_Database/SQL
 
-psql -h db -U postgres -d SPOT_Gothenburg -a -f init.sql
+psql -h db -U postgres -d $POSTGRES_DB -a -f init.sql
