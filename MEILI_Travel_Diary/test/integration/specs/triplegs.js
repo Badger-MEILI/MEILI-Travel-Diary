3079df9271f97f595aefbd788419edd100f0802f
adi <acpr@kth.se>
2016-10-22 16:38:01 +0200

    added tripleg transition insertion unit tests for unsuccesfull calls

--- MEILI_Travel_Diary/test/integration/specs/triplegs.js
+++ MEILI_Travel_Diary/test/integration/specs/triplegs.js
@@ -1,6 +1,6 @@
 function testTriplegs() {
 
-  describe("Triplegs", function() {
+  describe("Triplegs error calls", function() {
       it("insert transition between triplegs should fail due to invalid start time later than end time", function(done) {
 
           var timeDiff = 60*60*1000; // 1hour
@@ -21,6 +21,26 @@ function testTriplegs() {
               });
       });
 
+      it("insert transition between triplegs should fail due to wrong parameter state", function(done) {
+
+          var timeDiff = 60*60*1000; // 1hour
+          var tripleg = trip.triplegs[0];
+
+          trip.insertTransitionBetweenTriplegs(
+              tripleg.getStartTime().getTime() + timeDiff,
+              tripleg.getEndTime().getTime() - timeDiff,
+              undefined,
+              null
+          ).done(function(result) {
+                  done(new Error("Invalid input parameters should not result in a succesfull call"));
+              }).fail(
+              function (jqXHR, textStatus, errorThrown) {
+                  if (jqXHR.responseJSON.error.code != 400) return done(new Error("Status should be 400"));
+                  expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                  done();
+              });
+      });
+
       it("the specified transition poi id should not be null", function(done) {
 
           var tripleg = trip.triplegs[0];
@@ -37,5 +57,36 @@ function testTriplegs() {
               });
       });
 
+      it("the specified transition poi id should not be undefined", function(done) {
+
+          var tripleg = trip.triplegs[0];
+
+          tripleg.updateTransitionPoiIdOfTripleg(
+              undefined
+          ).done(function(result) {
+                  done(new Error("the specified transition poi id should not be allowed to be undefined"));
+              }).fail(
+              function (jqXHR, textStatus, errorThrown) {
+                  if (jqXHR.responseJSON.error.code != 400) return done(new Error("Status should be 400"));
+                  expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                  done();
+              });
+      });
+
+      it("the specified transition poi id should not be empty", function(done) {
+
+          var tripleg = trip.triplegs[0];
+
+          tripleg.updateTransitionPoiIdOfTripleg(
+              ''
+          ).done(function(result) {
+                  done(new Error("the specified transition poi id should not be allowed to be empty"));
+              }).fail(
+              function (jqXHR, textStatus, errorThrown) {
+                  if (jqXHR.responseJSON.error.code != 400) return done(new Error("Status should be 400"));
+                  expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                  done();
+              });
+      });
   });
 }
