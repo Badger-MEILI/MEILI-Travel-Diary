6af89d8d24e9169d583bcf8f1956be560494f74c
sweco-semhul <martin.hultman@sweco.se>
2016-10-24 08:42:13 +0200

    Make sure database init stops on first error to simplify problem solving

--- containers/controller/init.sh
+++ containers/controller/init.sh
@@ -3,4 +3,4 @@ rm -rf /MEILI_Database
 git clone https://github.com/Badger-MEILI/MEILI-Database /MEILI_Database
 cd /MEILI_Database/SQL
 
-psql -h db -U postgres -d $POSTGRES_DB -a -f init.sql
+psql -v ON_ERROR_STOP=1 -h db -U postgres -d $POSTGRES_DB -a -f init.sql
