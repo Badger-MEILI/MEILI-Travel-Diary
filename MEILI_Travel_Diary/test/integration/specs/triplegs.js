4b82eab8f8151e6a218737cc6e0a124114dc0219
sweco-semhul <martin.hultman@sweco.se>
2016-10-25 11:33:21 +0200

    Moved trips and tiplegs into separate describe to make test results easier to read

--- MEILI_Travel_Diary/test/integration/specs/triplegs.js
+++ MEILI_Travel_Diary/test/integration/specs/triplegs.js
@@ -1,212 +1,214 @@
 function testTriplegs() {
+    describe("Triplegs", function() {
 
-    describe("Insert transition between triplegs", function() {
+        describe("Insert transition between triplegs", function() {
 
-        it("insert transition between triplegs", function (done) {
+            it("insert transition between triplegs", function (done) {
 
-            var timeDiff = 5 * 60 * 1000; // 5 minutes
-            var tripleg = trip.triplegs[0];
-            var numberOfTriplegsBeforeAdd = trip.triplegs.length;
+                var timeDiff = 5 * 60 * 1000; // 5 minutes
+                var tripleg = trip.triplegs[0];
+                var numberOfTriplegsBeforeAdd = trip.triplegs.length;
 
-            trip.insertTransitionBetweenTriplegs(
-                tripleg.getStartTime().getTime() + timeDiff,
-                tripleg.getStartTime().getTime() + (timeDiff*2),
-                tripleg.mode[0].id,
-                tripleg.mode[0].id
-            ).done(function (trip) {
-                expect(trip.triplegs.length).to.be.equal(numberOfTriplegsBeforeAdd+1);
-                done();
+                trip.insertTransitionBetweenTriplegs(
+                    tripleg.getStartTime().getTime() + timeDiff,
+                    tripleg.getStartTime().getTime() + (timeDiff*2),
+                    tripleg.mode[0].id,
+                    tripleg.mode[0].id
+                ).done(function (trip) {
+                    expect(trip.triplegs.length).to.be.equal(numberOfTriplegsBeforeAdd+1);
+                    done();
+                });
             });
-        });
 
-        it("insert transition between triplegs should fail due to invalid start time later than end time", function (done) {
+            it("insert transition between triplegs should fail due to invalid start time later than end time", function (done) {
 
-            var timeDiff = 60 * 60 * 1000; // 1hour
-            var tripleg = trip.triplegs[0];
+                var timeDiff = 60 * 60 * 1000; // 1hour
+                var tripleg = trip.triplegs[0];
 
-            trip.insertTransitionBetweenTriplegs(
-                tripleg.getStartTime().getTime() + timeDiff,
-                tripleg.getEndTime().getTime() - timeDiff,
-                tripleg.mode[0].id,
-                tripleg.mode[0].id
-            ).done(function (result) {
-                done(new Error("Start time of transition should not be allowed to be later than the end time of the transition"));
-            }).fail(
-                function (jqXHR, textStatus, errorThrown) {
+                trip.insertTransitionBetweenTriplegs(
+                    tripleg.getStartTime().getTime() + timeDiff,
+                    tripleg.getEndTime().getTime() - timeDiff,
+                    tripleg.mode[0].id,
+                    tripleg.mode[0].id
+                ).done(function (result) {
+                    done(new Error("Start time of transition should not be allowed to be later than the end time of the transition"));
+                }).fail(
+                    function (jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.responseJSON.error.code != 400) {
+                            return done(new Error("Status should be 400"));
+                        }
+                        expect(jqXHR.responseJSON.error.msg).to.be.equal('Start time cannot be later than end time');
+                        done();
+                    });
+            });
+
+
+            it("insert transition between triplegs should fail due to invalid parameters", function(done) {
+
+                var timeDiff = 60*60*1000; // 1hour
+                var tripleg = trip.triplegs[0];
+
+                trip.insertTransitionBetweenTriplegs(
+                    tripleg.getStartTime().getTime() + timeDiff,
+                    tripleg.getEndTime().getTime() - timeDiff,
+                    undefined,
+                    null
+                ).done(function(result) {
+                    done(new Error("Invalid input parameters should not result in a succesfull call"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
                     if (jqXHR.responseJSON.error.code != 400) {
                         return done(new Error("Status should be 400"));
                     }
-                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Start time cannot be later than end time');
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
                     done();
                 });
-        });
-
-
-        it("insert transition between triplegs should fail due to invalid parameters", function(done) {
-
-            var timeDiff = 60*60*1000; // 1hour
-            var tripleg = trip.triplegs[0];
-
-            trip.insertTransitionBetweenTriplegs(
-                tripleg.getStartTime().getTime() + timeDiff,
-                tripleg.getEndTime().getTime() - timeDiff,
-                undefined,
-                null
-            ).done(function(result) {
-                done(new Error("Invalid input parameters should not result in a succesfull call"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
-            });
-        });
-    });
-
-    describe("Specify transition poi", function(){
-
-        it("the specified transition poi id should not be null", function(done) {
-
-            var tripleg = trip.triplegs[0];
-
-            tripleg.updateTransitionPoiIdOfTripleg(
-                null
-            ).done(function(result) {
-                done(new Error("the specified transition poi id should not be allowed to be null"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
             });
         });
 
-        it("the specified transition poi id should not be undefined", function(done) {
+        describe("Specify transition poi", function(){
 
-            var tripleg = trip.triplegs[0];
+            it("the specified transition poi id should not be null", function(done) {
 
-            tripleg.updateTransitionPoiIdOfTripleg(
-              undefined
-            ).done(function(result) {
-                done(new Error("the specified transition poi id should not be allowed to be undefined"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateTransitionPoiIdOfTripleg(
+                    null
+                ).done(function(result) {
+                    done(new Error("the specified transition poi id should not be allowed to be null"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+            it("the specified transition poi id should not be undefined", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateTransitionPoiIdOfTripleg(
+                  undefined
+                ).done(function(result) {
+                    done(new Error("the specified transition poi id should not be allowed to be undefined"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+            it("the specified transition poi id should not be empty", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateTransitionPoiIdOfTripleg(
+                    ''
+                ).done(function(result) {
+                    done(new Error("the specified transition poi id should not be allowed to be empty"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+
+          it("the specified transition poi id should exist", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateTransitionPoiIdOfTripleg(
+                    0
+                ).done(function(result) {
+                    done(new Error("the specified transition poi id should exists"));
+                }).fail(
+                    function (jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.responseJSON.error.code != 500) {
+                            return done(new Error("Status should be 500"));
+                        }
+                        expect(jqXHR.responseJSON.error.msg).to.not.be.null;
+                        done();
+                    });
             });
         });
 
-        it("the specified transition poi id should not be empty", function(done) {
 
-            var tripleg = trip.triplegs[0];
+        describe("Specify the travel mode of a tripleg", function(){
 
-            tripleg.updateTransitionPoiIdOfTripleg(
-                ''
-            ).done(function(result) {
-                done(new Error("the specified transition poi id should not be allowed to be empty"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
+            it("the specified travel mode should not be null", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateMode(
+                    null
+                ).done(function(result) {
+                    done(new Error("the specified travel mode should not be allowed to be null"));
+                }).fail(
+                    function (jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.responseJSON.error.code != 400) { 
+                            return done(new Error("Status should be 400"));
+                        }
+                        expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                        done();
+                    });
             });
-        });
+
+            it("the specified travel mode should not be undefined", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateMode(
+                    undefined
+                ).done(function(result) {
+                    done(new Error("the specified travel mode should not be allowed to be undefined"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+            it("the specified travel mode should not be empty", function(done) {
+
+                var tripleg = trip.triplegs[0];
+
+                tripleg.updateMode(
+                    ''
+                ).done(function(result) {
+                    done(new Error("the specified travel mode should not be allowed to be empty"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
 
 
-      it("the specified transition poi id should exist", function(done) {
+            it("the specified travel mode should exist", function(done) {
 
-            var tripleg = trip.triplegs[0];
+                var tripleg = trip.triplegs[0];
 
-            tripleg.updateTransitionPoiIdOfTripleg(
-                0
-            ).done(function(result) {
-                done(new Error("the specified transition poi id should exists"));
-            }).fail(
-                function (jqXHR, textStatus, errorThrown) {
+                tripleg.updateMode(
+                    0
+                ).done(function(result) {
+                    done(new Error("the specified travel mode should exists"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
                     if (jqXHR.responseJSON.error.code != 500) {
                         return done(new Error("Status should be 500"));
                     }
                     expect(jqXHR.responseJSON.error.msg).to.not.be.null;
                     done();
                 });
-        });
-    });
-
-
-    describe("Specify the travel mode of a tripleg", function(){
-
-        it("the specified travel mode should not be null", function(done) {
-
-            var tripleg = trip.triplegs[0];
-
-            tripleg.updateMode(
-                null
-            ).done(function(result) {
-                done(new Error("the specified travel mode should not be allowed to be null"));
-            }).fail(
-                function (jqXHR, textStatus, errorThrown) {
-                    if (jqXHR.responseJSON.error.code != 400) { 
-                        return done(new Error("Status should be 400"));
-                    }
-                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                    done();
-                });
-        });
-
-        it("the specified travel mode should not be undefined", function(done) {
-
-            var tripleg = trip.triplegs[0];
-
-            tripleg.updateMode(
-                undefined
-            ).done(function(result) {
-                done(new Error("the specified travel mode should not be allowed to be undefined"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
-            });
-        });
-
-        it("the specified travel mode should not be empty", function(done) {
-
-            var tripleg = trip.triplegs[0];
-
-            tripleg.updateMode(
-                ''
-            ).done(function(result) {
-                done(new Error("the specified travel mode should not be allowed to be empty"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
-            });
-        });
-
-
-        it("the specified travel mode should exist", function(done) {
-
-            var tripleg = trip.triplegs[0];
-
-            tripleg.updateMode(
-                0
-            ).done(function(result) {
-                done(new Error("the specified travel mode should exists"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 500) {
-                    return done(new Error("Status should be 500"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.not.be.null;
-                done();
             });
         });
     });
--- MEILI_Travel_Diary/test/integration/specs/trips.js
+++ MEILI_Travel_Diary/test/integration/specs/trips.js
@@ -1,6 +1,5 @@
 
 function testTrips() {
-
     describe("Trips", function() {
         it("get last trip for user should return a trip with triplegs", function(done) {
             user.getLastTrip()
@@ -11,60 +10,24 @@ function testTrips() {
                 done();
             });
         });
-    });
 
-    describe("Trip poi specification", function() {
 
-        it("undefined destination poi id should receive server error", function(done) {
-            trip.updateDestinationPoiIdOfTrip(
-                undefined
-            ).done(function (result) {
-                done(new Error("a request with an undefined destination poi id should not return a successfull response"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
+        describe("Trip poi specification", function() {
+
+            it("Update destination poi id should return status true", function(done) {
+                trip.updateDestinationPoiIdOfTrip(
+                    trip.getPlaces()[0].gid
+                ).done(function (result) {
+                    expect(result.status).to.be.equal(true);
+                    done();
+                });
             });
-        });
 
-        it("null destination poi id should receive server error", function(done) {
-            trip.updateDestinationPoiIdOfTrip(
-                null
-            ).done(function (result) {
-                done(new Error("a request with a null destination poi id should not return a successfull response"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
-            });
-        });
-
-        it("non existent destination poi id should receive server error", function(done) {
-            trip.updateDestinationPoiIdOfTrip(
-                0
-            ).done(function (result) {
-                    done(new Error("a request with a non existent destination poi id should not return a successfull response"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 500)  {
-                    return done(new Error("Status should be 500"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.not.be.null;
-                done();
-            });
-        });
-    });
-
-    describe("Trip purpose specification", function() {
-
-        it("undefined purpose id should receive server error", function(done) {
-            trip.updatePurposeOfTrip(
-                undefined
-            ).done(function (result) {
-                    done(new Error("a request with an undefined purpose id should not return a successfull response"));
+            it("undefined destination poi id should receive server error", function(done) {
+                trip.updateDestinationPoiIdOfTrip(
+                    undefined
+                ).done(function (result) {
+                    done(new Error("a request with an undefined destination poi id should not return a successfull response"));
                 }).fail(function (jqXHR, textStatus, errorThrown) {
                     if (jqXHR.responseJSON.error.code != 400) {
                         return done(new Error("Status should be 400"));
@@ -72,35 +35,89 @@ function testTrips() {
                     expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
                     done();
                 });
-        });
+            });
 
-        it("null purpose id should receive server error", function(done) {
-            trip.updatePurposeOfTrip(
-                null
-            ).done(function (result) {
-                done(new Error("a request with a null purpose id should not return a successfull response"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 400) {
-                    return done(new Error("Status should be 400"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
-                done();
+            it("null destination poi id should receive server error", function(done) {
+                trip.updateDestinationPoiIdOfTrip(
+                    null
+                ).done(function (result) {
+                    done(new Error("a request with a null destination poi id should not return a successfull response"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+            it("non existent destination poi id should receive server error", function(done) {
+                trip.updateDestinationPoiIdOfTrip(
+                    0
+                ).done(function (result) {
+                        done(new Error("a request with a non existent destination poi id should not return a successfull response"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 500)  {
+                        return done(new Error("Status should be 500"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.not.be.null;
+                    done();
+                });
             });
         });
 
-        it("non existent purpose id should receive server error", function(done) {
-            trip.updatePurposeOfTrip(
-                0
-            ).done(function (result) {
-                done(new Error("a request with a non existent purpose id should not return a successfull response"));
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                if (jqXHR.responseJSON.error.code != 500) {
-                    return done(new Error("Status should be 500"));
-                }
-                expect(jqXHR.responseJSON.error.msg).to.not.be.null;
-                done();
+        describe("Trip purpose specification", function() {
+
+            it("Update purpose should return status true", function(done) {
+                trip.updatePurposeOfTrip(
+                    trip.getPurposes()[0].id
+                ).done(function (result) {
+                    expect(result.status).to.be.equal(true);
+                    done();
+                });
+            });
+
+            it("undefined purpose id should receive server error", function(done) {
+                trip.updatePurposeOfTrip(
+                    undefined
+                ).done(function (result) {
+                        done(new Error("a request with an undefined purpose id should not return a successfull response"));
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.responseJSON.error.code != 400) {
+                            return done(new Error("Status should be 400"));
+                        }
+                        expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                        done();
+                    });
+            });
+
+            it("null purpose id should receive server error", function(done) {
+                trip.updatePurposeOfTrip(
+                    null
+                ).done(function (result) {
+                    done(new Error("a request with a null purpose id should not return a successfull response"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 400) {
+                        return done(new Error("Status should be 400"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.be.equal('Invalid input parameters');
+                    done();
+                });
+            });
+
+            it("non existent purpose id should receive server error", function(done) {
+                trip.updatePurposeOfTrip(
+                    0
+                ).done(function (result) {
+                    done(new Error("a request with a non existent purpose id should not return a successfull response"));
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseJSON.error.code != 500) {
+                        return done(new Error("Status should be 500"));
+                    }
+                    expect(jqXHR.responseJSON.error.msg).to.not.be.null;
+                    done();
+                });
             });
         });
     });
-
 }
