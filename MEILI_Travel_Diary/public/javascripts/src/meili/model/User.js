694b4150f7f43ab7f1147ab258764d62ed6ffc69
sweco-semhul <martin.hultman@sweco.se>
2016-10-27 14:59:01 +0200

    Adding a start for trip navigation on user

--- MEILI_Travel_Diary/public/javascripts/src/meili/model/User.js
+++ MEILI_Travel_Diary/public/javascripts/src/meili/model/User.js
@@ -15,24 +15,79 @@ User.prototype = {
     return api.trips.getNumberOfTrips(this.userId);
   },
 
-  getLastTrip: function() {
+  getPreviousTrip: function(tripId) {
     var dfd = $.Deferred();
-    api.trips.getLast(this.userId)
+    api.trips.navigateToPreviousTrip(this.userId)
       .done(function(trip) {
-        this.currentTrip = new Trip(trip);
-        api.triplegs.get(this.currentTrip.getId())
-          .done(function(result) {
-            this.currentTrip.updateTriplegs(result.triplegs);
-            dfd.resolve(this.currentTrip);
-          }.bind(this))
-          .fail(function(err) {
-            dfd.reject(err);
-          });
+        // init trip into Trip object and load triplegs
+        this._initTrip(tripJson)
+          .done(function(trip) {
+            // Set current trip and resolve
+            this.currentTrip = trip;
+            dfd.resolve(trip);
+          }.bind(this));
       }.bind(this))
       .fail(function(err) {
-            dfd.reject(err);
-          });
+        dfd.reject(err);
+      });
 
     return dfd.promise();
+  },
+
+  getNextTrip: function(tripId) {
+    var dfd = $.Deferred();
+    api.trips.navigateToNextTrip(this.userId)
+      .done(function(trip) {
+        // init trip into Trip object and load triplegs
+        this._initTrip(tripJson)
+          .done(function(trip) {
+            // Set current trip and resolve
+            this.currentTrip = trip;
+            dfd.resolve(trip);
+          }.bind(this));
+      }.bind(this))
+      .fail(function(err) {
+        dfd.reject(err);
+      });
+
+    return dfd.promise();
+  },
+
+  getLastTrip: function() {
+    var dfd = $.Deferred();
+    // Get last trip from api
+    api.trips.getLast(this.userId)
+      .done(function(tripJson) {
+        // init trip into Trip object and load triplegs
+        this._initTrip(tripJson)
+          .done(function(trip) {
+            // Set current trip and resolve
+            this.currentTrip = trip;
+            dfd.resolve(trip);
+          }.bind(this));
+      }.bind(this))
+      .fail(function(err) {
+        dfd.reject(err);
+      });
+
+    return dfd.promise();
+  },
+
+  getTriplegsForTrip: function(trip) {
+    var dfd = $.Deferred();
+    api.triplegs.get(trip.getId())
+      .done(function(result) {
+        trip.updateTriplegs(result.triplegs);
+        dfd.resolve(trip);
+      })
+      .fail(function(err) {
+        dfd.reject(err);
+      });
+    return dfd.promise();
+  },
+
+  _initTrip: function(tripJson) {
+    var trip = new Trip(tripJson);
+    return this.getTriplegsForTrip(trip);
   }
 };
