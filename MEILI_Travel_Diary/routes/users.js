01a8d5196e320a39ffd6bc6a67211c75383ac67d
adi <acpr@kth.se>
2016-10-27 10:01:43 +0200

    modified the api endpoint for ios location updates

--- MEILI_Travel_Diary/routes/users.js
+++ MEILI_Travel_Diary/routes/users.js
@@ -235,7 +235,7 @@ router.post('/insertLog',  function(req, res) {
 router.post('/insertLocationsIOS',  function(req, res) {
 
     var data =  JSON.parse(req.body.dataToUpload);
-    var sql = "INSERT INTO location_table (upload,  accuracy_, altitude_, bearing_, lat_, lon_, time_, speed_, satellites_, user_id, size, totalismoving, totalmax, totalmean, totalmin,"+
+    var sql = "INSERT INTO raw_data.location_table (upload,  accuracy_, altitude_, bearing_, lat_, lon_, time_, speed_, satellites_, user_id, size, totalismoving, totalmax, totalmean, totalmin,"+
         "totalnumberofpeaks, totalnumberofsteps, totalstddev, "+
         "xismoving, xmaximum, xmean, xminimum, xnumberofpeaks, xstddev,"+
         "yismoving, ymax, ymean, ymin, ynumberofpeaks, ystddev, zismoving, zmax, zmean, zmin, znumberofpeaks, zstddev, "+
