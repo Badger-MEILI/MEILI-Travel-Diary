281344d7639fd603d1329bdc0f85d02969044b64
sweco-semhul <martin.hultman@sweco.se>
2016-11-07 22:17:14 +0100

    Test transportation poi

--- MEILI_Travel_Diary/test/integration/specs/pois.js
+++ MEILI_Travel_Diary/test/integration/specs/pois.js
@@ -24,20 +24,68 @@ function testPois() {
                     // Set it on trip
                     trip.updateDestinationPoiIdOfTrip(result.insert_destination_poi).done(function() {
                         // Get last trip to verify
-                        user.getLastTrip().done(function(trip) {
+                        user.getLastTrip().done(function(updatedTrip) {
 
                             // Make sure trip have destinations
-                            expect(trip.destination_places).to.not.be.equal(null);
+                            expect(updatedTrip.destination_places).to.not.be.equal(null);
 
                             var isOnTrip = false;
                             // loop trip to find inserted destination
-                            for (var i = 0; i < trip.destination_places.length; i++) {
-                                var poi = trip.destination_places[i];
+                            for (var i = 0; i < updatedTrip.destination_places.length; i++) {
+                                var poi = updatedTrip.destination_places[i];
                                 if(poi.name === nameToInsert && poi.latitude === pointToInsert.lat && poi.longitude === pointToInsert.lng) {
                                     isOnTrip = true;
                                 }
                             };
                             expect(isOnTrip).to.be.equal(true);
+                            done();
+                        });
+                    });
+                }).fail(function(err) {
+                    done(err);
+                });
+
+            });
+
+        });
+
+        describe("insert transportation poi", function() {
+
+            it("should be able to insert a transportation poi", function (done) {
+                var name = 'Test';
+                var point = { lat: 59, lng: 18 };
+
+                api.pois.insertTransportationPoi(name, point, user.id).done(function (result) {
+                    expect(result.insert_transition_poi).to.not.be.null;
+                    done();
+                }).fail(function(err) {
+                    done(err);
+                });
+            });
+
+            it("should be able to insert transportation poi and set it on tripleg", function (done) {
+                var nameToInsert = 'Test';
+                var pointToInsert = { lat: 59, lng: 18 };
+
+                // Insert new
+                api.pois.insertTransportationPoi(nameToInsert, pointToInsert, user.id).done(function (result) {
+                    // Set it on trip
+                    trip.getFirstTripleg().updateTransitionPoiIdOfTripleg(result.insert_transition_poi).done(function() {
+                        // Get last trip to verify
+                        user.getLastTrip().done(function(updatedTriptrip) {
+                            var updatedTripleg = updatedTrip.getFirstTripleg();
+                            // Make sure trip have destinations
+                            expect(updatedTripleg.places).to.not.be.equal(null);
+
+                            var isOnTripleg = false;
+                            // loop trip to find inserted destination
+                            for (var i = 0; i < trip.places.length; i++) {
+                                var poi = updatedTripleg.places[i];
+                                if(poi.name === nameToInsert && poi.lat === pointToInsert.lat && poi.lon === pointToInsert.lng) {
+                                    isOnTripleg = true;
+                                }
+                            };
+                            expect(isOnTripleg).to.be.equal(true);
                             done();
                         });
                     });
