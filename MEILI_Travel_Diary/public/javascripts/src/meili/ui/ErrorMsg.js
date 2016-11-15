dff53764eca4649912996a0df27e6bae0b64f07b
sweco-semhul <martin.hultman@sweco.se>
2016-11-15 11:04:35 +0100

    Make sure its only one error message and that its removed after 10 seconds

--- MEILI_Travel_Diary/public/javascripts/src/meili/ui/ErrorMsg.js
+++ MEILI_Travel_Diary/public/javascripts/src/meili/ui/ErrorMsg.js
@@ -5,10 +5,24 @@ var ErrorMsg = ErrorMsg || function(elementId) {
 };
 
 ErrorMsg.prototype = {
+  errMsgElmtId: 'error-message',
+  timerHide: null,
+
   show: function(msg) {
-    $(this.elementId).prepend('<div id="error-message"class="alert alert-danger alert-dismissible" role="alert">' +
+    this.hide();
+    $(this.elementId).prepend('<div id="' + this.errMsgElmtId + '" class="alert alert-danger alert-dismissible" role="alert">' +
       '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-      '<strong class="type">Fel</strong> <span class="msg">' + msg + '</span>' +
+      '<strong class="type">Error</strong> <span class="msg">' + msg + '</span>' +
     '</div>');
+    if(this.timerHide) {
+      clearTimeout(this.timerHide);
+    }
+    this.timerHide = setTimeout(function() {
+      this.hide();
+    }.bind(this), 10000);
+  },
+
+  hide: function() {
+    $('#'+this.errMsgElmtId).remove();
   }
 };
