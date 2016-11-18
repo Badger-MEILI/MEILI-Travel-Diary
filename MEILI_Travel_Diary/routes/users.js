7afbf2112f67cf2b5c52a40c4a67cfafd71ee732
adi <acpr@kth.se>
2016-11-18 06:51:31 +0100

    added logging for ios upload to identify the user=0 error

--- MEILI_Travel_Diary/routes/users.js
+++ MEILI_Travel_Diary/routes/users.js
@@ -194,6 +194,9 @@ router.post('/insertLog',  function(req, res) {
 router.post('/insertLocationsIOS',  function(req, res) {
 
     var data =  JSON.parse(req.body.dataToUpload);
+
+    console.log(req.body.dataToUpload);
+
     var sql = "INSERT INTO raw_data.location_table (upload,  accuracy_, altitude_, bearing_, lat_, lon_, time_, speed_, satellites_, user_id, size, totalismoving, totalmax, totalmean, totalmin,"+
         "totalnumberofpeaks, totalnumberofsteps, totalstddev, "+
         "xismoving, xmaximum, xmean, xminimum, xnumberofpeaks, xstddev,"+
